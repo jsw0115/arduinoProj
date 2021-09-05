a8de82050e27b44c5654abc0ec95a602a2c21bb1
jsw0115 <87688825+jsw0115@users.noreply.github.com>
2021-09-05 14:19:21 +0900

    Update sketch_aug31a.ino

--- sketch_aug31a.ino
+++ sketch_aug31a.ino
@@ -12,19 +12,13 @@ void setup(){
  pinMode(trigPin, OUTPUT);
  pinMode(echoPin, INPUT);
 }
- 
-//초음파를 보낸다. 다 보내면 echo가 HIGH(신호받기) 상태로 대기
 
 void loop(){
   float duration, distance;
   digitalWrite(trigPin, HIGH);
   delay(10);
   digitalWrite(trigPin, LOW);
- 
-  // echoPin 이 HIGH를 유지한 시간을 저장한다.
   duration = pulseIn(echoPin, HIGH);
-  // HIGH였을 때 시간(초음파가 보냈다가 다시 들어온 시간)을 가지고 거리를 계산한다.
-  // 340은 초당 초음파(소리)의 속도, 10000은 밀리세컨드를 세컨드로, 왕복거리이므로 2로 나눠준다.
   distance = ((float)(340 * duration) / 10000) / 2;
 
  Serial.print("\nDIstance:");
